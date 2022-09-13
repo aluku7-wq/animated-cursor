8ec46c07335233635d5cf3ba9dc8a4ff628be0df
aluku godrick <foreveraluku@gmail.com>
2022-09-13 20:46:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import { useState } from "react";
+import "./App.css";
+import Cursor from "./components/Cursor";
+
+const App = () => {
+  const [scaling, setscaling] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Cursor scaling={scaling} />
+      <section>
+        <button
+          onMouseEnter={() => setscaling(true)}
+          onMouseLeave={() => setscaling(false)}>
+          read more
+        </button>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
